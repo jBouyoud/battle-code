7ff46fc81961edcc6eb8a15325f0f83faf51ad78
JULIEN BOUYOUD <julien-externe.bouyoud@edf.fr>
2016-03-10 20:38:20 +0100

    client refactoring

--- src/test/java/fr/battle/undefined/LauncherTest.java
+++ src/test/java/fr/battle/undefined/LauncherTest.java
@@ -2,20 +2,10 @@ package fr.battle.undefined;
 
 import java.io.IOException;
 import java.net.ProxySelector;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
-
 import fr.battle.undefined.util.Constants;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * http://52.29.48.22:8080/
@@ -26,8 +16,8 @@ import lombok.extern.slf4j.Slf4j;
 public class LauncherTest {
 
 	private static final String SERVER = "52.29.48.22";
-	private static final long MAX_TEAM_ID = Constants.TEAMID + 6;
 	private static final int SOCKET_NUMBER = 2160;
+	private static final long MAX_TEAM_ID = Constants.TEAMID + 6;
 
 	public static void main(final String[] zero) throws IOException,
 			URISyntaxException, InterruptedException {
@@ -36,24 +26,15 @@ public class LauncherTest {
 		final SystemDefaultRoutePlanner routePlanner = new SystemDefaultRoutePlanner(
 				ProxySelector.getDefault());
 		// Creation la partie
-		try (
-				final CloseableHttpClient httpclient = HttpClients.custom()
-						.setRoutePlanner(routePlanner).build()) {
-
-			final URI uri = new URIBuilder().setScheme("http").setHost(SERVER
-					+ ":8080/test").setPath("/createBattle").setParameter(
-							"teamId", Long.toString(Constants.TEAMID))
-					.setParameter("secret", Constants.SECRET).build();
-
-			final HttpGet httpget = new HttpGet(uri);
-			final ResponseHandler<String> handler = new BasicResponseHandler();
-			final long gameId = Long.parseLong(httpclient.execute(httpget,
-					handler));
-			LOGGER.info("{}", gameId);
+		try (	RestClient restClient = new RestClient(SERVER)) {
+			final long gameId = restClient.createGame();
+			if (gameId == -1) {
+				return;
+			}
 
 			final Properties prop = new Properties();
-			prop.load(LauncherTest.class.getResourceAsStream(
-					"/test.properties"));
+			prop.load(LauncherTest.class
+					.getResourceAsStream("/test.properties"));
 
 			final String[] ias = prop.getProperty("ias").split(",");
 
@@ -68,10 +49,10 @@ public class LauncherTest {
 						try {
 							new Client(SERVER, teamId, SOCKET_NUMBER, gameId,
 									(IA) Class.forName(className).newInstance())
-											.init(1).start();
-						} catch (InstantiationException | IllegalAccessException
-								| ClassNotFoundException
-								| InterruptedException e) {
+									.init(1).start();
+						} catch (InstantiationException
+								| IllegalAccessException
+								| ClassNotFoundException | InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
@@ -86,35 +67,14 @@ public class LauncherTest {
 				return;
 			}
 			// Demarrage de la game
-			// http://xxxxxx:8080/test/startBattle?gameId=xxxx&teamId=10&secret=bobsecret
-			final URI startUri = new URIBuilder().setScheme("http").setHost(
-					SERVER + ":8080").setPath("/test/startBattle").setParameter(
-							"gameId", Long.toString(gameId)).setParameter(
-									"teamId", Long.toString(Constants.TEAMID))
-					.setParameter("secret", Constants.SECRET).build();
-
-			final HttpGet startGet = new HttpGet(startUri);
-			final ResponseHandler<String> handler2 = new BasicResponseHandler();
-			httpclient.execute(startGet, handler2);
+			restClient.startGame(gameId);
 
 			// Pour voir le jeu
-			LOGGER.info(
-					"To Stop the battle : http://{}:8080/test/stopBattle?gameId={}&teamId={}&secret={}",
-					new Object[] { SERVER, gameId, Constants.TEAMID,
-							Constants.SECRET });
 
 			System.in.read();
 
-			final URI stopUri = new URIBuilder().setScheme("http").setHost(
-					SERVER + ":8080").setPath("/test/stopBattle").setParameter(
-							"gameId", Long.toString(gameId)).setParameter(
-									"teamId", Long.toString(Constants.TEAMID))
-					.setParameter("secret", Constants.SECRET).build();
-
-			final HttpGet stopGet = new HttpGet(stopUri);
-			final ResponseHandler<String> handler3 = new BasicResponseHandler();
-			httpclient.execute(stopGet, handler3);
-			// http://xxxxxx:8080/?gameId=votre game Id
+			restClient.stopGame(gameId);
 		}
 	}
+
 }
---
+++ src/test/java/fr/battle/undefined/RestClient.java
@@ -0,0 +1,67 @@
+package fr.battle.undefined;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import fr.battle.undefined.util.Constants;
+
+@Slf4j
+@RequiredArgsConstructor
+public class RestClient implements Closeable {
+
+	private final String server;
+	private final CloseableHttpClient httpclient = HttpClients.createDefault();
+
+	public long createGame() throws IOException, URISyntaxException {
+		final URI uri = new URIBuilder().setScheme("http").setHost(server + ":8080/test").setPath("/createBattle")
+				.setParameter("teamId", Long.toString(Constants.TEAMID)).setParameter("secret", Constants.SECRET)
+				.build();
+
+		final HttpGet httpget = new HttpGet(uri);
+		final ResponseHandler<String> handler = new BasicResponseHandler();
+		final long gameId = Long.parseLong(httpclient.execute(httpget, handler));
+		LOGGER.info("{}", gameId);
+		return gameId;
+	}
+
+	public void startGame(final long gameId) throws IOException, URISyntaxException {
+		// http://xxxxxx:8080/test/startBattle?gameId=xxxx&teamId=10&secret=bobsecret
+		final URI startUri = new URIBuilder().setScheme("http").setHost(server + ":8080").setPath("/test/startBattle")
+				.setParameter("gameId", Long.toString(gameId)).setParameter("teamId", Long.toString(Constants.TEAMID))
+				.setParameter("secret", Constants.SECRET).build();
+
+		final HttpGet startGet = new HttpGet(startUri);
+		final ResponseHandler<String> handler2 = new BasicResponseHandler();
+		httpclient.execute(startGet, handler2);
+
+		LOGGER.info("To Stop the battle : http://{}:8080/test/stopBattle?gameId={}&teamId={}&secret={}", new Object[] {
+				server, gameId, Constants.TEAMID, Constants.SECRET });
+	}
+
+	public void stopGame(final long gameId) throws IOException, URISyntaxException {
+		final URI stopUri = new URIBuilder().setScheme("http").setHost(server + ":8080").setPath("/test/stopBattle")
+				.setParameter("gameId", Long.toString(gameId)).setParameter("teamId", Long.toString(Constants.TEAMID))
+				.setParameter("secret", Constants.SECRET).build();
+
+		final HttpGet stopGet = new HttpGet(stopUri);
+		final ResponseHandler<String> handler3 = new BasicResponseHandler();
+		httpclient.execute(stopGet, handler3);
+	}
+
+	@Override
+	public void close() throws IOException {
+		httpclient.close();
+	}
+}
